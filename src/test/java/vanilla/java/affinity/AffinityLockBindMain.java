613e0a8f4541b471463142d618d88c0ac74c6824
Peter Lawrey <peter.lawrey@gmail.com>
2012-01-13 14:17:54 +0000

    Support binding of a whole core for hyper-threaded systems. AffinityLock.acquireCore()

--- src/test/java/vanilla/java/affinity/AffinityLockBindMain.java
+++ src/test/java/vanilla/java/affinity/AffinityLockBindMain.java
@@ -28,14 +28,17 @@ public class AffinityLockBindMain {
             // find a cpu on a different socket, otherwise a different core.
             AffinityLock readerLock = al.acquireLock(DIFFERENT_SOCKET, DIFFERENT_CORE);
             new Thread(new SleepRunnable(readerLock, false), "reader").start();
+
             // find a cpu on the same core, or the same socket, or any free cpu.
             AffinityLock writerLock = readerLock.acquireLock(SAME_CORE, SAME_SOCKET, ANY);
             new Thread(new SleepRunnable(writerLock, false), "writer").start();
+
             Thread.sleep(200);
         } finally {
             al.release();
         }
-// re-use the same cpu for the engine.
+
+        // allocate a whole core to the engine so it doesn't have to compete for resources.
         al = AffinityLock.acquireCore(false);
         new Thread(new SleepRunnable(al, true), "engine").start();
 
@@ -43,11 +46,11 @@ public class AffinityLockBindMain {
         System.out.println("\nThe assignment of CPUs is\n" + AffinityLock.dumpLocks());
     }
 
-    private static class SleepRunnable implements Runnable {
+    static class SleepRunnable implements Runnable {
         private final AffinityLock affinityLock;
         private final boolean wholeCore;
 
-        public SleepRunnable(AffinityLock affinityLock, boolean wholeCore) {
+        SleepRunnable(AffinityLock affinityLock, boolean wholeCore) {
             this.affinityLock = affinityLock;
             this.wholeCore = wholeCore;
         }
